d381f538a81cee5e391f98f69667666acdaab649
Bidhata <pandey.bidhata58@gmail.com>
2022-03-09 22:31:18 +0545

    test data to add in db

---
+++ routes/dbdata.js
@@ -0,0 +1,126 @@
+
+
+db.users.insertMany([
+  {
+      name:"bidhata 123",
+      rate:"20",
+      address:"pokhara, lakeside 123",
+      contact:"9860123456",
+      
+      isCertified: true, // false means local guide
+      languages:["english","german"],
+      reviews:
+      [
+        {
+          reviewer:"email@email",
+          review:"very nice"
+        },
+        {
+          reviewer:"email2@email",
+          review:"very nice2"
+        }
+      ],
+      description:"I love travelling and food and bla and bla and bla and bla and bla and bla and bla and bla and bla and bla and bla and bla and bla and bla and bla and bla and bla and bla "
+    },
+    {
+      name:"bidhata 123",
+      rate:"20",
+      address:"pokhara, lakeside 123",
+      contact:"9860123456",
+      
+      isCertified: true, // false means local guide
+      languages:["english","german"],
+      reviews:
+      [
+        {
+          reviewer:"email@email",
+          review:"very nice"
+        },
+        {
+          reviewer:"email2@email",
+          review:"very nice2"
+        }
+      ],
+      description:"I love travelling and food and bla and bla and bla and bla and bla and bla and bla and bla and bla and bla and bla and bla and bla and bla and bla and bla and bla and bla "
+    },{
+      name:"bidhata 123",
+      rate:"20",
+      address:"pokhara, lakeside 123",
+      contact:"9860123456",
+      
+      isCertified: true, // false means local guide
+      languages:["english","german"],
+      reviews:
+      [
+        {
+          reviewer:"email@email",
+          review:"very nice"
+        },
+        {
+          reviewer:"email2@email",
+          review:"very nice2"
+        }
+      ],
+      description:"I love travelling and food and bla and bla and bla and bla and bla and bla and bla and bla and bla and bla and bla and bla and bla and bla and bla and bla and bla and bla "
+    },{
+      name:"bidhata 123",
+      rate:"20",
+      address:"pokhara, lakeside 123",
+      contact:"9860123456",
+      
+      isCertified: true, // false means local guide
+      languages:["english","german"],
+      reviews:
+      [
+        {
+          reviewer:"email@email",
+          review:"very nice"
+        },
+        {
+          reviewer:"email2@email",
+          review:"very nice2"
+        }
+      ],
+      description:"I love travelling and food and bla and bla and bla and bla and bla and bla and bla and bla and bla and bla and bla and bla and bla and bla and bla and bla and bla and bla "
+    },{
+      name:"bidhata 123",
+      rate:"20",
+      address:"pokhara, lakeside 123",
+      contact:"9860123456",
+      
+      isCertified: true, // false means local guide
+      languages:["english","german"],
+      reviews:
+      [
+        {
+          reviewer:"email@email",
+          review:"very nice"
+        },
+        {
+          reviewer:"email2@email",
+          review:"very nice2"
+        }
+      ],
+      description:"I love travelling and food and bla and bla and bla and bla and bla and bla and bla and bla and bla and bla and bla and bla and bla and bla and bla and bla and bla and bla "
+    },{
+      name:"bidhata 123",
+      rate:"20",
+      address:"pokhara, lakeside 123",
+      contact:"9860123456",
+      
+      isCertified: true, // false means local guide
+      languages:["english","german"],
+      reviews:
+      [
+        {
+          reviewer:"email@email",
+          review:"very nice"
+        },
+        {
+          reviewer:"email2@email",
+          review:"very nice2"
+        }
+      ],
+      description:"I love travelling and food and bla and bla and bla and bla and bla and bla and bla and bla and bla and bla and bla and bla and bla and bla and bla and bla and bla and bla "
+    },
+])
